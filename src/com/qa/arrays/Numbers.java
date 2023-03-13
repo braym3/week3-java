efe9c8e2f0eaf0c84561cdf817da8847ee25e92c
Admin <Admin@192.168.1.100>
2023-03-13 16:49:41 +0000

    Fixed word converter method

--- src/com/qa/arrays/Numbers.java
+++ src/com/qa/arrays/Numbers.java
@@ -19,9 +19,13 @@ public class Numbers {
 		return sum;
 	}
 
-	public static void convertToWord(int num) {
+	public static String convertToWord(int num) {
 		ArrayList<Integer> digits = new ArrayList<Integer>();
 		int n;
+
+		if (num == 0) {
+			digits.add(0);
+		}
 		// Add each digit to the digits arrayList
 		while (num > 0) {
 			// Get the last digit
@@ -31,17 +35,19 @@ public class Numbers {
 			// Divide the number by 10 (to get the next digit in the next loop)
 			num = num / 10;
 		}
-		String[] singleDigits = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+		String[] singleDigits = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 		String[] tens = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
 		String[] teens = { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
 				"eighteen", "nineteen" };
 		String word = "";
+
 		// Loop through digits array - backwards (the first digit is at the end of the
 		// array)
 		// Match corresponding word to the digit - add to word
 		for (int i = digits.size() - 1; i >= 0; i--) {
 			// if second-to-last digit - add teen or tens number as word
-			if (i == 1) {
+			if (i == 1 && digits.get(i) != 0) {
 				// if penultimate digit is a 1 - add corresponding teen number and break
 				if (digits.get(i) == 1) {
 					word = word + teens[digits.get(i - 1)];
@@ -49,8 +55,10 @@ public class Numbers {
 				}
 				word = word + tens[digits.get(i) - 2];
 			} else {
-				word = word + singleDigits[digits.get(i) - 1];
-
+				// skip zeros (unless that is the only number)
+				if (!(digits.get(i) == 0 && digits.size() > 1)) {
+					word = word + singleDigits[digits.get(i)];
+				}
 				// if it's 3rd to last digit - add hundred after word
 				if (i == 2) {
 					word = word + "-hundred";
@@ -59,14 +67,12 @@ public class Numbers {
 					word = word + "-thousand";
 				}
 			}
-
 			// add a dash between words
-			if (i != 0) {
+			if (i != 0 && digits.get(i - 1) != 0) {
 				word = word + "-";
 			}
-
 		}
-		System.out.println(word);
+		return word;
 	}
 
 	public static void main(String[] args) {
@@ -75,12 +81,12 @@ public class Numbers {
 		System.out.println(addDigits(3462));
 		System.out.println(addDigits(11));
 
-		convertToWord(35);
-		convertToWord(17);
-		convertToWord(167);
-		convertToWord(528);
-		convertToWord(3546);
-		convertToWord(9999);
+		System.out.println();
+
+		for (int i = 0; i <= 999; i++) {
+			System.out.println(convertToWord(i));
+		}
+
 	}
 
 }
