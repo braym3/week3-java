f23e561d4a6800e2700052db61a9b37e90ba87ce
Admin <Admin@192.168.1.100>
2023-03-13 11:57:05 +0000

    Created iteration flowchart 2

---
+++ src/com/qa/flowchart/IterationFlowchart2.java
@@ -0,0 +1,15 @@
+package com.qa.flowchart;
+
+public class IterationFlowchart2 {
+
+	public static void main(String[] args) {
+		for (int a = 100; a <= 200; a++) {
+			if (a % 2 == 0) {
+				System.out.println("-");
+			} else {
+				System.out.println("*");
+			}
+		}
+	}
+
+}
