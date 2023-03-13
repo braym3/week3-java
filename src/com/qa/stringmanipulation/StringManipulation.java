4b55ecd43e6f085b1a6ed15c52a3d1650288265b
Admin <Admin@192.168.1.100>
2023-03-13 13:02:13 +0000

    Added word count method

---
+++ src/com/qa/stringmanipulation/StringManipulation.java
@@ -0,0 +1,25 @@
+package com.qa.stringmanipulation;
+
+public class StringManipulation {
+
+	public static int wordCount(String s) {
+		int count = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			// if a space is found or if it is the last character of the last word - add 1
+			// to word count
+			if (s.charAt(i) == ' ' || (i == s.length() - 1)) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(wordCount("Hello world how are you?"));
+		System.out.println(wordCount("This is an example java exercise"));
+		System.out.println(wordCount("Hello"));
+		System.out.println(wordCount("Hello world "));
+	}
+
+}
