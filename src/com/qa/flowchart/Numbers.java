972a969a6a095a8e6645ea833154dc4ae4205ff6
Admin <Admin@192.168.1.100>
2023-03-13 12:07:03 +0000

    Added second number iteration method

--- src/com/qa/flowchart/Numbers.java
+++ src/com/qa/flowchart/Numbers.java
@@ -11,8 +11,16 @@ public class Numbers {
 		}
 	}
 
+	public static void printNTimes(int n) {
+		for (int i = 1; i <= n; i++) {
+			System.out.println(n);
+		}
+	}
+
 	public static void main(String[] args) {
 		printNumbers();
+		printNTimes(10);
+		printNTimes(3);
 	}
 
 }
