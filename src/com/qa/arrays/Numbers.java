742755c4f1fbab554b67c07c54f3d21c26ddac1a
Admin <Admin@192.168.1.100>
2023-03-13 16:01:42 +0000

    Finished word conversion task

--- src/com/qa/arrays/Numbers.java
+++ src/com/qa/arrays/Numbers.java
@@ -31,9 +31,8 @@ public class Numbers {
 			// Divide the number by 10 (to get the next digit in the next loop)
 			num = num / 10;
 		}
-		System.out.println(digits);
 		String[] singleDigits = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-		String[] tens = { "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+		String[] tens = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
 		String[] teens = { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
 				"eighteen", "nineteen" };
 		String word = "";
@@ -41,12 +40,26 @@ public class Numbers {
 		// array)
 		// Match corresponding word to the digit - add to word
 		for (int i = digits.size() - 1; i >= 0; i--) {
-			// if last digit - add single digit number as word
-			if (i == 0) {
+			// if second-to-last digit - add teen or tens number as word
+			if (i == 1) {
+				// if penultimate digit is a 1 - add corresponding teen number and break
+				if (digits.get(i) == 1) {
+					word = word + teens[digits.get(i - 1)];
+					break;
+				}
+				word = word + tens[digits.get(i) - 2];
+			} else {
 				word = word + singleDigits[digits.get(i) - 1];
-			} else if (i == 1) {
-				word = word + tens[digits.get(i) - 1];
+
+				// if it's 3rd to last digit - add hundred after word
+				if (i == 2) {
+					word = word + "-hundred";
+					// if it's 4th to last digit - add thousand after word
+				} else if (i == 3) {
+					word = word + "-thousand";
+				}
 			}
+
 			// add a dash between words
 			if (i != 0) {
 				word = word + "-";
@@ -63,6 +76,11 @@ public class Numbers {
 		System.out.println(addDigits(11));
 
 		convertToWord(35);
+		convertToWord(17);
+		convertToWord(167);
+		convertToWord(528);
+		convertToWord(3546);
+		convertToWord(9999);
 	}
 
 }
