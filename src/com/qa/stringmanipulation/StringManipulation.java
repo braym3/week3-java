d912f48e0fa09bb91885f0e99f30721550f3a831
Admin <Admin@192.168.1.100>
2023-03-14 16:40:36 +0000

    Added to strings task 2

--- src/com/qa/stringmanipulation/StringManipulation.java
+++ src/com/qa/stringmanipulation/StringManipulation.java
@@ -16,32 +16,48 @@ public class StringManipulation {
 	}
 
 	public static void printVertical(String s) {
-		int lastSpaceIndex = 0;
+//		int lastSpaceIndex = 0;
+//		for (int i = 0; i < s.length(); i++) {
+//			// if a space is found or if it is the last character of the last word
+//			if (s.charAt(i) == ' ' || (i == s.length() - 1)) {
+//				System.out.println(s.substring(lastSpaceIndex, i));
+//				lastSpaceIndex = i + 1;
+//			}
+//		}
+
+		// simpler version - print all chars and make new line when there's a space
 		for (int i = 0; i < s.length(); i++) {
-			// if a space is found or if it is the last character of the last word
-			if (s.charAt(i) == ' ' || (i == s.length() - 1)) {
-				System.out.println(s.substring(lastSpaceIndex, i + 1));
-				lastSpaceIndex = i + 1;
+			if (s.charAt(i) == ' ') {
+				System.out.print("\n");
+			} else {
+				System.out.print(s.charAt(i));
 			}
+
 		}
 	}
 
 	public static void printReverseVertical(String s) {
-		int lastSpaceIndex = s.length() - 1;
+		int lastSpaceIndex = s.length();
 		for (int i = s.length() - 1; i >= 0; i--) {
-			// if a space is found or if it is the last character of the last word
+			// if a space is found or if it is the last character of the first word
 			if (s.charAt(i) == ' ') {
 				System.out.println(s.substring(i + 1, lastSpaceIndex));
 				lastSpaceIndex = i;
 			} else if (i == 0) {
 				System.out.println(s.substring(i, lastSpaceIndex));
-				lastSpaceIndex = i;
 			}
 		}
 	}
 
 	public static boolean findSubstring(String message, String sub) {
-		boolean found = true;
+		boolean found = false;
+
+		/**
+		 * // recursive? - look for first matching char - then next, repeat for(int i =
+		 * 0; i <= message.length() - 1; i++) { // if fisrt char of sub matches message
+		 * if(message.charAt(i) == sub.charAt(0)) { findSubstring((message.substring(i,
+		 * message.length()-1)), sub.substring(i)) } }
+		 **/
 		return found;
 	}
 
@@ -55,6 +71,7 @@ public class StringManipulation {
 
 		printVertical("This is an example java exercise");
 
+		System.out.println();
 		System.out.println();
 
 		printReverseVertical("This is an example java exercise");
