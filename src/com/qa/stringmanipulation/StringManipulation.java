26c505e4260aac107afadb2a411da109c1dfeb11
Admin <Admin@192.168.1.100>
2023-03-14 15:57:55 +0000

    Adding to string manipulation exercises

--- src/com/qa/stringmanipulation/StringManipulation.java
+++ src/com/qa/stringmanipulation/StringManipulation.java
@@ -15,11 +15,49 @@ public class StringManipulation {
 		return count;
 	}
 
+	public static void printVertical(String s) {
+		int lastSpaceIndex = 0;
+		for (int i = 0; i < s.length(); i++) {
+			// if a space is found or if it is the last character of the last word
+			if (s.charAt(i) == ' ' || (i == s.length() - 1)) {
+				System.out.println(s.substring(lastSpaceIndex, i + 1));
+				lastSpaceIndex = i + 1;
+			}
+		}
+	}
+
+	public static void printReverseVertical(String s) {
+		int lastSpaceIndex = s.length() - 1;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			// if a space is found or if it is the last character of the last word
+			if (s.charAt(i) == ' ') {
+				System.out.println(s.substring(i + 1, lastSpaceIndex));
+				lastSpaceIndex = i;
+			} else if (i == 0) {
+				System.out.println(s.substring(i, lastSpaceIndex));
+				lastSpaceIndex = i;
+			}
+		}
+	}
+
+	public static boolean findSubstring(String message, String sub) {
+		boolean found = true;
+		return found;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(wordCount("Hello world how are you?"));
 		System.out.println(wordCount("This is an example java exercise"));
 		System.out.println(wordCount("Hello"));
 		System.out.println(wordCount("Hello world "));
+
+		System.out.println();
+
+		printVertical("This is an example java exercise");
+
+		System.out.println();
+
+		printReverseVertical("This is an example java exercise");
 	}
 
 }
