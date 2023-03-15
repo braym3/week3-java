5d5a496a59a4fc6a0ec496381ac5ea17a02037b1
Admin <Admin@192.168.1.100>
2023-03-15 10:38:46 +0000

    Added accessors/mutators for person

--- src/com/qa/oop/Person.java
+++ src/com/qa/oop/Person.java
@@ -19,4 +19,28 @@ public class Person {
 		return s;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 }
